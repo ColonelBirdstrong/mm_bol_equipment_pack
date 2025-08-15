c0480cc7737f2c323f1a5fe1a735cd04dc674960
Colonel Birdstrong <34130979+ColonelBirdstrong@users.noreply.github.com>
2025-08-14 20:04:14 -0600

    More models added
    Great Fairy's Sword: Burning Dragon Sword
    Stone Mask: JoJo's Bizarre Adventure Stone Mask
    Mirror Shield: Improved OoT Mirror Shield and ALTTP Mirror Shield

--- src/birdequipment.c
+++ src/birdequipment.c
@@ -14,6 +14,8 @@
 #include "gBoLRedShield.h"
 
 // Mirror Shield
+#include "gBoLImprovedMirrorShield.h"
+#include "gBoLALTTPMirrorShield.h"
 
 // Kokiri Sword
 
@@ -26,11 +28,16 @@
 #include "gBoLMoonlightGreatsword.h"
 #include "gBoLPursuerUGS.h"
 #include "gBoLUltimaWeapon.h"
+#include "gBoLBurningDragonsword.h"
+
+// Masks
+#include "gJoJoStoneMask.h"
 
 DECLARE_ROM_SEGMENT(object_link_child);
 DECLARE_ROM_SEGMENT(gameplay_keep);
 DECLARE_ROM_SEGMENT(icon_item_static_yar);
 DECLARE_ROM_SEGMENT(object_mir_ray);
+DECLARE_ROM_SEGMENT(object_mask_stone);
 
 // Shields
 extern Gfx* gPlayerShields[];
@@ -51,6 +58,10 @@ extern Gfx gLinkHumanGildedSwordMtx;
 extern Gfx gLinkHumanGreatFairysSwordDL[];
 extern Gfx gLinkHumanGreatFairysSwordMtx;
 
+// Masks
+extern Gfx* D_801C0B20[];
+extern Gfx object_mask_stone_DL_000820[];
+
 enum BirdHeroShieldModel {
     HERO_SHIELD_OFF,
     HERO_SHIELD_MM3D_GI,
@@ -67,6 +78,18 @@ enum BirdGreatFairySwordModel {
     GREATFAIRYSWORD_MOONLIGHTGREATSWORD,
     GREATFAIRYSWORD_PURSUERUGS,
     GREATFAIRYSWORD_ULTIMAWEAPON,
+    GREATFAITYSWORD_BURNINGDRAGONSWORD,
+};
+
+enum BirdStoneMaskModel {
+    STONEMASK_OFF,
+    STONEMASK_JOJO,
+};
+
+enum BirdMirrorShieldModel {
+    MIRRORSHIELD_OFF,
+    MIRRORSHIELD_OOTIMPROVED,
+    MIRRORSHIELD_ALTTP,
 };
 
 void* gRam;
@@ -87,48 +110,74 @@ RECOMP_HOOK_RETURN("DmaMgr_ProcessRequest") void after_dma() {
     if (gVrom == SEGMENT_ROM_START(object_link_child)) {
         uintptr_t old_segment_6 = gSegments[0x06];
         gSegments[0x06] = OS_K0_TO_PHYSICAL(gRam);
-        Gfx* to_patch = Lib_SegmentedToVirtual(gLinkHumanHerosShieldDL);
+        Gfx* to_patch_heroshield = Lib_SegmentedToVirtual(gLinkHumanHerosShieldDL);
         switch (recomp_get_config_u32("bird_heroshield_model"))
         {
         case HERO_SHIELD_OFF:
             ;
             break;
         case HERO_SHIELD_MM3D_GI:
-        gSPBranchList(to_patch , gBoLMM3DHeroShield);
+        gSPBranchList(to_patch_heroshield , gBoLMM3DHeroShield);
             break;
         case HERO_SHIELD_KITESHIELD:
-        gSPBranchList(to_patch , gBoLKiteShield);
+        gSPBranchList(to_patch_heroshield , gBoLKiteShield);
             break;
         case HERO_SHIELD_OOT3D:
-        gSPBranchList(to_patch , gBoLOoT3DHyShield);
+        gSPBranchList(to_patch_heroshield , gBoLOoT3DHyShield);
             break;
         case HERO_SHIELD_STSHIELD:
-        gSPBranchList(to_patch , gBoLSTShield);
+        gSPBranchList(to_patch_heroshield , gBoLSTShield);
             break;
         case HERO_SHIELD_ALTTPFIGHTERSHIELD:
-        gSPBranchList(to_patch , gBoLFighterShield);
+        gSPBranchList(to_patch_heroshield , gBoLFighterShield);
             break;
         case HERO_SHIELD_ALTTPREDSHIELD:
-        gSPBranchList(to_patch , gBoLRedShield);
+        gSPBranchList(to_patch_heroshield , gBoLRedShield);
             break;
         }
-        Gfx* to_patch_2 = Lib_SegmentedToVirtual(gLinkHumanGreatFairysSwordDL);
+        
+        Gfx* to_patch_gfs = Lib_SegmentedToVirtual(gLinkHumanGreatFairysSwordDL);
         switch (recomp_get_config_u32("bird_greatfairysword_model"))
         {
         case GREATFAIRYSWORD_OFF:
             ;
             break;
         case GREATFAIRYSWORD_DS1CLAYMORE:
-        gSPBranchList(to_patch_2 , gBoLDS1Claymore);
+        gSPBranchList(to_patch_gfs , gBoLDS1Claymore);
             break;
         case GREATFAIRYSWORD_MOONLIGHTGREATSWORD:
-        gSPBranchList(to_patch_2 , gBoLMoonlightGreatsword);
+        gSPBranchList(to_patch_gfs , gBoLMoonlightGreatsword);
             break;
         case GREATFAIRYSWORD_PURSUERUGS:
-        gSPBranchList(to_patch_2 , gBoLPursuerUGS);
+        gSPBranchList(to_patch_gfs , gBoLPursuerUGS);
             break;
         case GREATFAIRYSWORD_ULTIMAWEAPON:
-        gSPBranchList(to_patch_2 , gBoLUltimaWeapon);
+        gSPBranchList(to_patch_gfs , gBoLUltimaWeapon);
+            break;
+        case GREATFAITYSWORD_BURNINGDRAGONSWORD:
+        gSPBranchList(to_patch_gfs , gBoLBurningDragonsword);
+            break;
+        }
+        switch (recomp_get_config_u32("bird_stonemask_model"))
+            {
+        case STONEMASK_OFF:
+        D_801C0B20[PLAYER_MASK_STONE - 1] = object_mask_stone_DL_000820;
+            break;
+        case STONEMASK_JOJO: //Credit to Xenthos for the original model!
+        D_801C0B20[PLAYER_MASK_STONE - 1] = gJoJoStoneMask;
+            break;
+        }
+        Gfx* to_patch_mirrorshield = Lib_SegmentedToVirtual(gLinkHumanMirrorShieldDL);
+        switch (recomp_get_config_u32("bird_mirrorshield_model"))
+        {
+        case MIRRORSHIELD_OFF:
+            ;
+            break;
+        case MIRRORSHIELD_OOTIMPROVED:
+        gSPBranchList(to_patch_mirrorshield , gBoLImprovedMirrorShield);
+            break;
+        case MIRRORSHIELD_ALTTP:
+        gSPBranchList(to_patch_mirrorshield , gBoLALTTPMirrorShield);
             break;
         }
         gSegments[0x06] = old_segment_6;
@@ -136,3 +185,7 @@ RECOMP_HOOK_RETURN("DmaMgr_ProcessRequest") void after_dma() {
     gVrom = 0;
     gRam = NULL;
 }
+
+// RECOMP_HOOK("Player_Init") void on_Player_Init() {
+//     
+// }
